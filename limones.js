90f15c1ff22962acff21ba9a1478c5f21e159276
mathiasmunoz1 <munozmathias540@gmail.com>
2026-05-27 21:36:59 -0500

    agregar setinterval

--- limones.js
+++ limones.js
@@ -14,8 +14,10 @@ let limonX=canvas.width/2;
 let limonY=0;
 let puntaje=0;
 let vidas=3;
+let velocidadCaida=200;
 
 function iniciar(){
+    setInterval(bajarLimon,velocidadCaida);
     dibujarSuelo();
     dibujarPersonaje();
     aparecerLimon();
